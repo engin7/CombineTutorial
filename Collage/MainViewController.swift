2985b001f6b644c6bc7f8fda9344896bd5d6d7ca
Engin KUK <kuk.engin@gmail.com>
2021-07-19 17:06:28 +0300

    Initial Commit

---
+++ Collage/MainViewController.swift
@@ -0,0 +1,57 @@
+import Combine
+import UIKit
+
+class MainViewController: UIViewController {
+  
+  // MARK: - Outlets
+
+  @IBOutlet weak var imagePreview: UIImageView! {
+    didSet {
+      imagePreview.layer.borderColor = UIColor.gray.cgColor
+    }
+  }
+  @IBOutlet weak var buttonClear: UIButton!
+  @IBOutlet weak var buttonSave: UIButton!
+  @IBOutlet weak var itemAdd: UIBarButtonItem!
+
+  // MARK: - Private properties
+  
+
+  // MARK: - View controller
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let collageSize = imagePreview.frame.size
+    
+  }
+  
+  private func updateUI(photos: [UIImage]) {
+    buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
+    buttonClear.isEnabled = photos.count > 0
+    itemAdd.isEnabled = photos.count < 6
+    title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
+  }
+  
+  // MARK: - Actions
+  
+  @IBAction func actionClear() {
+    
+  }
+  
+  @IBAction func actionSave() {
+    guard let image = imagePreview.image else { return }
+    
+  }
+  
+  @IBAction func actionAdd() {
+    
+  }
+  
+  private func showMessage(_ title: String, description: String? = nil) {
+    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { alert in
+      self.dismiss(animated: true, completion: nil)
+    }))
+    present(alert, animated: true, completion: nil)
+  }
+}
